895256fd184194587ba060cbf3c6b54d885c23b1
Simon Jackle James <hailfallkompany@gmail.com>
2018-01-03 12:50:37 +0700

    game description

---
+++ for Review.cpp
@@ -0,0 +1,71 @@
+Villages is an Open-World Sandbox game that have rules to encourage players to make complex society and economy.
+
+
+Villages is a game that combine interesting mechanics from many other sandbox games. 
+It aims to let players face variety of gameplay and scenario by conduct a few basics rules.
+
+
+
+KEY FEATURES
+
+Economy, the game world will have limited resource so any player will not get to the same resource and amount.
+Players then will form an economy by trading system in the game.
+
+Manufacturing, from hand-crafted to factory-made you can make a product with different tiers.
+Each tier vary in cost and benefit, and make dynamic to the whole economy.
+
+Construction, differ from traditional sandbox game. Villages will let you construct a building as if in real life, by using construction site, construction tools and vehicles.
+
+Skill-Based, every player can choose their skill to advanced but with limited skill points.
+So you must balance your skill branches and benefit from it.
+Everyone will depend on other player's ability and form their society.
+
+
+Other Planned Features
+
+Clan and Company System
+Law Enforcement
+Mining
+Fishing
+Farming
+Hunting
+Vehicles
+Weapons
+Stores
+
+and Warzone Map (new gamemode)
+
+
+
+
+------------------------------
+
+[b]Villages[/b] is a game that combine interesting mechanics from many other sandbox games. 
+It aims to let players face variety of gameplay and scenario by conduct a few basics rules.
+
+[b][h2]KEY FEATURES[/h2][/b]
+[list]Economy, the game world will have limited resource so any player will not get to the same resource and amount.
+Players then will form an economy by trading system in the game.[/list]
+
+[list]Manufacturing, from hand-crafted to factory-made you can make a product with different tiers.
+Each tier vary in cost and benefit, and make dynamic to the whole economy.[/list]
+
+[list]Construction, differ from traditional sandbox game. Villages will let you construct a building as if in real life, by using construction site, construction tools and vehicles.[/list]
+
+[list]Skill-Based, every player can choose their skill to advanced but with limited skill points.
+So you must balance your skill branches and benefit from it.
+Everyone will depend on other player's ability and form their society.[/list]
+
+[h2]Other Planned Features[/h2]
+[list]Clan and Company System[/list]
+[list]Law Enforcement[/list]
+[list]Mining[/list]
+[list]Fishing[/list]
+[list]Farming[/list]
+[list]Hunting[/list]
+[list]Vehicles[/list]
+[list]Weapons[/list]
+[list]Stores[/list]
+
+and Warzone Map (new gamemode)
+
