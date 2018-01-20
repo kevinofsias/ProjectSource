a1577aa09b6cde8aa07f577e8abc22a6c2a910d6
Simon Jackle James <hailfallkompany@gmail.com>
2018-01-20 13:18:17 +0700

    itendef

---
+++ LootboxDef.cpp
@@ -0,0 +1,21 @@
+{
+
+"appid" : 749820,
+"items" : [
+
+	{
+	"itemdefid" : 1,
+	"type" : "item",
+	"name" : "Disaster Relief Box",
+	"description" : "When disaster happen, this box will help you a lot.",
+	"name_color" : "",
+	"background_color" : "",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/7QkVegY.png",
+	"icon_url_large": "https://i.imgur.com/F7Hh6rh.png"	
+	}
+
+]
+
+}
