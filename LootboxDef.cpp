14678f7098de56731aba58b2613faf88f189ebc7
Simon Jackle James <hailfallkompany@gmail.com>
2018-01-20 15:35:47 +0700

    a

--- LootboxDef.cpp
+++ LootboxDef.cpp
@@ -2,18 +2,40 @@
 
 "appid" : 749820,
 "items" : [
+	
+	{
+	"itemdefid" : 0,
+	"type" : "item",
+	"name" : "z",
+	"hidden" : true
+	},
+
+	{
+	"itemdefid" : 247,
+	"type" : "item",
+	"name" : "z",
+	"hidden" : true
+	},
+
+	{
+	"itemdefid" : 70000,
+	"type" : "item",
+	"name" : "z",
+	"hidden" : true
+	},
 
 	{
 	"itemdefid" : 1,
 	"type" : "item",
+	"display_type" : "Item Package",
 	"name" : "Disaster Relief Box",
-	"description" : "When disaster happen, this box will help you a lot.",
-	"name_color" : "",
-	"background_color" : "",
+	"description" : "When a disaster happen, this box will help you a lot.",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
 	"tradable" : true,
 	"marketable" : true,
-	"icon_url": "https://i.imgur.com/7QkVegY.png",
-	"icon_url_large": "https://i.imgur.com/F7Hh6rh.png"	
+	"icon_url": "https://i.imgur.com/ZvrW4Tl.png",
+	"icon_url_large": "https://i.imgur.com/wkkhswf.png"	
 	}
 
 ]
