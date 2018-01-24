88b44a0da01826d61c729d0b772399937855d201
Simon Jackle James <hailfallkompany@gmail.com>
2018-01-24 07:27:50 +0700

    add placeable

---
+++ PlaceableDef.cpp
@@ -0,0 +1,22 @@
+{
+
+"appid" : 749820,
+"items" : [
+
+	{
+	"itemdefid" : 9000,
+	"type" : "item",
+	"display_type" : "Placeable",
+	"name" : "Camping Tent",
+	"description" : "The World is Home.",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/gsrNurd.png",
+	"icon_url_large": "https://i.imgur.com/gsrNurd.png"	
+	}
+
+]
+
+}
