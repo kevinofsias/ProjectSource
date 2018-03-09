455830ac4ed89f52256b107d3aec5b5cf1b42b05
Simon James Hailkamp <hailfallkompany@gmail.com>
2018-03-09 09:28:11 +0700

    new items

---
+++ Livestock.cpp
@@ -0,0 +1,26 @@
+{
+
+"appid" : 749820,
+"items" : [
+
+	{
+	"itemdefid" : 25000,
+	"type" : "item",
+	"promo" : "manual",
+	"display_type" : "Livestock",
+	"name" : "Cow",
+	"description" : "Moo Moo",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/vxf5OFz.png",
+	"icon_url_large": "https://i.imgur.com/vxf5OFz.png",
+	"drop_interval" : 1,
+	"use_drop_window" : true,
+	"drop_window" : "1440",
+	"drop_max_per_window" : "1000"
+	}
+]
+
+}
