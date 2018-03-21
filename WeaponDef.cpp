f9ad9ab8935e38574c0555b22d2e57729d209d42
Simon James Hailkamp <hailfallkompany@gmail.com>
2018-03-22 00:27:31 +0700

    clear weapons

--- WeaponDef.cpp
+++ WeaponDef.cpp
@@ -8,45 +8,41 @@
 	"type" : "item",
 	"display_type" : "Weapon",
 	"name" : "Tough Assault Rifle (ART47)",
-	"description" : "Unlikely to jammed since 1949",
+	"description" : "Unlikely to jammed since 1949\nAmmo : 762",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
 	"marketable" : true,
 	"icon_url": "https://i.imgur.com/TKVEcv9.png",
-	"icon_url_large": "https://i.imgur.com/TKVEcv9.png",
-	"allowed_tags_from_tools" : "mod"
+	"icon_url_large": "https://i.imgur.com/TKVEcv9.png"
 	},
 
 	{
 	"itemdefid" : 15001,
-	"type" : "tag_tool",
-	"display_type" : "Weapon Mod",
-	"name" : "Extended Mag",
-	"description" : "",
+	"type" : "item",
+	"display_type" : "Weapon",
+	"name" : "Precise Assault Rifle (ARP4)",
+	"description" : "More precise, Less ammo waste\nAmmo : 556",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
 	"marketable" : true,
-	"icon_url": "https://i.imgur.com/cgMTJI8.png",
-	"icon_url_large": "https://i.imgur.com/cgMTJI8.png",
-	"tags" : "mod : extendedmag",
-	"tags_to_remove_on_tool_use" : "mod"
+	"icon_url": "https://i.imgur.com/VpyZKUR.png",
+	"icon_url_large": "https://i.imgur.com/VpyZKUR.png"
 	},
 
 	{
 	"itemdefid" : 15002,
 	"type" : "item",
 	"display_type" : "Weapon",
-	"name" : "Precise Assault Rifle (ARP4)",
-	"description" : "",
+	"name" : "Grandpa's Punch (P45)",
+	"description" : "Ammo : 45",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
 	"marketable" : true,
-	"icon_url": "https://i.imgur.com/TKVEcv9.png",
-	"icon_url_large": "https://i.imgur.com/TKVEcv9.png",
-	"allowed_tags_from_tools" : "mod"
+	"icon_url": "https://i.imgur.com/oAUFdHX.png",
+	"icon_url_large": "https://i.imgur.com/oAUFdHX.png"
 	}
 
 ]
