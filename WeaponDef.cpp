edb595d2add52e29adfd8c2624b37a0c80dc070a
Simon James Hailkamp <hailfallkompany@gmail.com>
2018-03-22 13:59:27 +0700

    weapon

--- WeaponDef.cpp
+++ WeaponDef.cpp
@@ -8,7 +8,7 @@
 	"type" : "item",
 	"display_type" : "Weapon",
 	"name" : "Tough Assault Rifle (ART47)",
-	"description" : "Unlikely to jammed since 1949\nAmmo : 762",
+	"description" : "Unlikely to jammed since 1949\nAmmo : 7.62",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
@@ -22,7 +22,7 @@
 	"type" : "item",
 	"display_type" : "Weapon",
 	"name" : "Precise Assault Rifle (ARP4)",
-	"description" : "More precise, Less ammo waste\nAmmo : 556",
+	"description" : "More precise, Less ammo waste\nAmmo : 5.56",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
@@ -36,13 +36,41 @@
 	"type" : "item",
 	"display_type" : "Weapon",
 	"name" : "Grandpa's Punch (P45)",
-	"description" : "Ammo : 45",
+	"description" : "Ammo : .45",
 	"name_color" : "FFFFFF",
 	"background_color" : "DDA373",
 	"tradable" : true,
 	"marketable" : true,
 	"icon_url": "https://i.imgur.com/oAUFdHX.png",
 	"icon_url_large": "https://i.imgur.com/oAUFdHX.png"
+	},
+
+	{
+	"itemdefid" : 15003,
+	"type" : "item",
+	"display_type" : "Weapon",
+	"name" : "Grandma's Kick (SG1)",
+	"description" : "Ammo : Shotgun Shell",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/uw63XZ0.png",
+	"icon_url_large": "https://i.imgur.com/uw63XZ0.png"
+	},
+
+	{
+	"itemdefid" : 15004,
+	"type" : "item",
+	"display_type" : "Weapon",
+	"name" : "Deer Hunter (SNI40)",
+	"description" : "Ammo : .308",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/RCrOzn8.png",
+	"icon_url_large": "https://i.imgur.com/RCrOzn8.png"
 	}
 
 ]
