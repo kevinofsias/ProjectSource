18313784a9ed309b79656f4a21720ef0067fcbed
Simon James Hailkamp <hailfallkompany@gmail.com>
2018-03-23 09:53:30 +0700

    add roadster

---
+++ LandVehicleDef.cpp
@@ -0,0 +1,21 @@
+{
+
+"appid" : 749820,
+"items" : [
+
+	{
+	"itemdefid" : 6000,
+	"type" : "item",
+	"display_type" : "Land Vehicle",
+	"name" : "People's Roadster",
+	"description" : "[color=yellow]Pre-order[/color]\nCome with Automatic Transmission",
+	"name_color" : "FFFFFF",
+	"background_color" : "DDA373",
+	"tradable" : true,
+	"marketable" : true,
+	"icon_url": "https://i.imgur.com/fDrCzko.png",
+	"icon_url_large": "https://i.imgur.com/fDrCzko.png"
+	}
+]
+
+}
