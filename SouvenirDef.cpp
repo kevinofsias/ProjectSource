ce69a69bdb5a2d93293209dd76a8170f74e99ca5
Simon James Hailkamp <hailfallkompany@gmail.com>
2018-04-08 12:59:15 +0700

    Custom Souvenir

---
+++ SouvenirDef.cpp
@@ -0,0 +1,17 @@
+{
+
+"appid" : 749820,
+"items" : [
+
+	{
+	"itemdefid" : 28000,
+	"type" : "item",
+	"display_type" : "Souvenir",
+	"name" : "Till I die",
+	"description" : "Once you already know wassup be smart and play dumb.",
+	"icon_url": "https://i.imgur.com/duXgFco.png",
+	"icon_url_large": "https://i.imgur.com/duXgFco.png"
+	}
+]
+
+}
